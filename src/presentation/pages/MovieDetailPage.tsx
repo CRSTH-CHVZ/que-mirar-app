dd279cc8bd2cc0a1c899711defffd1c6cf6eeda9
Cristhian Chavez <crsth.1990@fpuna.edu.py>
2024-09-10 18:13:14 -0400

    detail page

---
+++ src/presentation/pages/MovieDetailPage.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { OmdbApiService } from '../../data/api/OmdbApiService';
+
+export const MovieDetailPage: React.FC = () => {
+    const { imdbID } = useParams<{ imdbID: string }>();
+    const [movie, setMovie] = useState<any>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchMovieDetails = async () => {
+            setLoading(true);
+            setError(null);
+
+            try {
+                const movieDetails = await OmdbApiService.getMovieDetails(imdbID!);
+                setMovie(movieDetails);
+            } catch (err) {
+                setError('Error fetching movie details');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchMovieDetails();
+    }, [imdbID]);
+
+    if (loading) return <p className="text-center text-gray-500">Loading...</p>;
+    if (error) return <p className="text-center text-red-500">{error}</p>;
+
+    return (
+        <div className="container mx-auto px-4 py-6">
+            {movie && (
+                <div className="bg-white shadow-md rounded-lg p-6">
+                    <h2 className="text-2xl font-semibold">{movie.Title}</h2>
+                    <img src={movie.Poster} alt={movie.Title} className="my-4 w-full max-w-xs mx-auto"/>
+                    <p><strong>Plot:</strong> {movie.Plot}</p>
+                    <p><strong>Genre:</strong> {movie.Genre}</p>
+                    <p><strong>Director:</strong> {movie.Director}</p>
+                    <p><strong>Actors:</strong> {movie.Actors}</p>
+                </div>
+            )}
+        </div>
+    );
+};
--- src/presentation/routes/AppRouter.tsx
+++ src/presentation/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SearchPage } from '../pages/SearchPage';
+import {MovieDetailPage} from "../pages/MovieDetailPage.tsx";
 
 export const AppRouter: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<SearchPage />} />
+                <Route path="/movie/:imdbID" element={<MovieDetailPage />} />
             </Routes>
         </Router>
     );
